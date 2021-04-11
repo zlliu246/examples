b58817104341dccbf99bad5982befbf3e9b989ad
liuzuolin1996 <liuzuolin1996@gmail.com>
2021-04-11 18:46:24 +0800

    added telebot code

---
+++ telegram-bot/run.py
@@ -0,0 +1,18 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
+
+# Telegram bot token goes here
+# IMPORTANT - remmeber to use .env files if you're committing to a public repository on github
+TOKEN="1728636860:AAGXUhptfiVlhziXmwziaARdvJE92-gruTI"
+
+def route(update, context):
+    text = update.message.text
+    update.message.reply_text(text)
+
+updater = Updater(TOKEN, use_context=True)
+updater.dispatcher.add_handler(MessageHandler(Filters.text, route))
+updater.start_polling()
+
+print("telegram bot is running!")
+
+updater.idle()
+
